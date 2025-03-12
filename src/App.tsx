13b2100a735c5cc1cb550322b7a32b6f080cdf06
Luca Bianconi <71259950+Bikappa@users.noreply.github.com>
2025-03-12 23:00:03 +0100

    feat: new wip page

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      style={{
+        background: "url('industrial.svg') no-repeat",
+        backgroundPosition: "left bottom",
+        backgroundSize: "cover",
+        color: "white",
+      }}
+      className="flex flex-col items-center justify-center w-screen h-screen"
+    >
+      <h2 className="font-semibold">BIKAPPA.COM</h2>
+      <div className="divider">
+        <progress className="progress w-56 progress-accent" />
+      </div>
+      <h3>This website is under construction</h3>
+    </div>
+  );
+}
+
+export default App;
